46477a921c8929ad6bd08b8c83ae38d7c898a992
Beto Martinez <betoml5@hotmail.com>
2020-12-13 16:08:27 -0600

    First commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// GUARDAS LOS TEXTOS EN CONSTANTES:
+// Y en cada click cambias el texto
+const texto =
+    "To decide which URL to load, the user agent examines each <source>'s srcset, media, and type attributes to select a compatible image that best matches the current layout and capabilities of the display device.";
