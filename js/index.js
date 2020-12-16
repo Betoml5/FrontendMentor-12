1a1fece254769576f99ff6752ba552c5fe77638f
Beto Martinez <betoml5@hotmail.com>
2020-12-16 13:59:19 -0600

    Images script done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
 // GUARDAS LOS TEXTOS EN CONSTANTES:
 // Y en cada click cambias el texto
-const texto =
-    "To decide which URL to load, the user agent examines each <source>'s srcset, media, and type attributes to select a compatible image that best matches the current layout and capabilities of the display device.";
+var i = 0;
+
+const btn_next = document.querySelector('#btn_next')
+const btn_back = document.querySelector('#btn_back')
+const container_main = document.querySelector('#container_main')
+const p_change = document.querySelector('#p_change');
+
+
+btn_next.addEventListener('click', () => {
+    i > 2 ? i = 0 : 
+    console.log('click');
+    container_main.style.backgroundImage= `url(${arrayImage[i]})`
+    i += 1;
+
+
+
+})
+
+btn_back.addEventListener('click', () => {
+    i < 0 ? i = 2 : 
+    console.log('click');
+    container_main.style.backgroundImage= `url(${arrayImage[i]})`
+    i -= 1;
+})
+
+const arrayImage = [
+    './images/desktop-image-hero-1.jpg',
+    './images/desktop-image-hero-2.jpg',
+    './images/desktop-image-hero-3.jpg'
+]
+
+
+    
